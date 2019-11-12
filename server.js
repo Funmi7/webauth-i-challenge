72d5ff975d54193e3f0e82ded03a5ef8cb3fb873
Funmi7 <talabifunmilayo@gmail.com>
2019-11-12 16:00:36 +0100

    Created a new session for successful login by a user

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const helmet = require('helmet');
-const server = express();
+const session = require('express-session');
 
+const authRouter = require('./auth/authRouter') 
 const userRouter = require('./users/userRouter');
 
+const server = express();
+
+const sessionConfig = {
+  name: 'notsession',
+  secret: 'you do not want to know the secret!',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: false
+  },
+  resave: false,
+  saveUninitialized: false,
+  // store: new knexSessionStore({
+  //   knex: require('./data/dbConfig')
+  // })
+}
 server.use(helmet());
 server.use(express.json());
+server.use(session(sessionConfig))
 
 server.get('/', (req, res) => {
   res.send('server running')
 })
 
-server.use('/api/users', userRouter)
+server.use('/api/auth', authRouter);
+server.use('/api/users', userRouter);
 
 module.exports = server;
